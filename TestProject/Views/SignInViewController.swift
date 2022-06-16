7d1618b82e758b36fb238fa2e0093f0c677d4a62
thecodeplayer <thecodeplayer23@gmail.com>
2022-06-16 17:05:51 +0800

    Added Home Module

--- TestProject/Views/SignInViewController.swift
+++ TestProject/Views/SignInViewController.swift
@@ -41,7 +41,10 @@ class SignInViewController: UIViewController {
     
     //Onclick
     @IBAction func signInValidation(_ sender: Any) {
-        validate()
+//        validate()
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let homePageViewController = storyBoard.instantiateViewController(withIdentifier: "HomePage") as! HomePageViewController
+        self.present(homePageViewController, animated: true, completion: nil)
     }
     
     //UITExtField Validations
